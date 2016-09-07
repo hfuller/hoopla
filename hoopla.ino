dfabcb77ef18a95e4a102609d71f2415979f35a9
Hunter Fuller <hfuller@pixilic.com>
2016-09-06 22:34:04 -0500

    add... basically the stock OTA example code

---
+++ hoopla.ino
@@ -0,0 +1,62 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#define VERSION		1
+
+const char* ssid = "";
+const char* password = "";
+
+void setup() {
+  delay(1000); //TODO debug
+
+  Serial.begin(115200);
+  Serial.print("hoopla v"); Serial.println(VERSION);
+
+  Serial.println("Starting wireless");
+  WiFi.mode(WIFI_STA);
+
+  Serial.print("Attempting to associate (STA) to "); Serial.println(WiFi.SSID());
+  WiFi.begin();
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Could not associate (STA)");
+    //delay(5000);
+    //ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle();
+}
