650df645a459166039b9be756f595ac7108ab62e
Hunter Fuller <hfuller@pixilic.com>
2016-09-21 00:34:30 -0500

    add LED functionality

--- hoopla.ino
+++ hoopla.ino
@@ -2,61 +2,88 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include "FastLED.h"
 
 #define VERSION		1
+#define DEBUG
+
+#define NUMPIXELS	1
+#define DATA_PIN	0 
+#define CLOCK_PIN	2
 
 const char* ssid = "";
 const char* password = "";
 
+CRGB leds[NUMPIXELS];
+
+bool updating = false;
+
 void setup() {
-  delay(1000); //TODO debug
+	
+	#ifdef debug
+	delay(1000); //TODO debug
+	#endif
 
-  Serial.begin(115200);
-  Serial.print("hoopla v"); Serial.println(VERSION);
+	Serial.begin(115200);
+	Serial.print("hoopla v"); Serial.println(VERSION);
 
-  Serial.println("Starting wireless");
-  WiFi.mode(WIFI_STA);
+	Serial.println("Starting LEDs");
+	FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUMPIXELS);
 
-  Serial.print("Attempting to associate (STA) to "); Serial.println(WiFi.SSID());
-  WiFi.begin();
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Could not associate (STA)");
-    //delay(5000);
-    //ESP.restart();
-  }
+	Serial.println("Starting wireless");
+	WiFi.mode(WIFI_STA);
 
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
+	Serial.print("Attempting to associate (STA) to "); Serial.println(WiFi.SSID());
+	WiFi.begin();
+	if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+		Serial.println("Could not associate (STA)");
+		//delay(5000);
+		//ESP.restart();
+	}
 
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
+	// Port defaults to 8266
+	// ArduinoOTA.setPort(8266);
 
-  // No authentication by default
-  // ArduinoOTA.setPassword((const char *)"123");
+	// Hostname defaults to esp8266-[ChipID]
+	// ArduinoOTA.setHostname("myesp8266");
 
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+	// No authentication by default
+	// ArduinoOTA.setPassword((const char *)"123");
+
+	Serial.println("Setting up OTA");
+	ArduinoOTA.onStart([]() {
+		Serial.println("Starting OTA update. Other functions will be suspended.");
+	});
+	ArduinoOTA.onEnd([]() {
+		Serial.println("\nOTA update complete. Reloading");
+	});
+	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+		Serial.printf("OTA progress: %u%%\r", (progress / (total / 100)));
+	});
+	ArduinoOTA.onError([](ota_error_t error) {
+		Serial.printf("OTA Error[%u]: ", error);
+		if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+		else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+		else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+		else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+		else if (error == OTA_END_ERROR) Serial.println("End Failed");
+	});
+	ArduinoOTA.begin();
+
+	Serial.print("Startup complete. IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  ArduinoOTA.handle();
+	
+	ArduinoOTA.handle();
+
+	Serial.println("Doing LED stuff");
+	leds[0] = CRGB::Green;
+	FastLED.show();
+	delay(500);
+	leds[0] = CRGB::Black;
+	FastLED.show();
+	delay(500);
+
 }
