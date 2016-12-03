f4e66d5bc4c4e0ab666acb26220685872444ccc7
Hunter Fuller <hfuller@pixilic.com>
2016-12-03 02:17:46 -0600

    remove manual reset from build script

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ NAME="hoopla-devkit"
 #terrible build script
 ~/Downloads/arduino-1.6.7/arduino hoopla.ino
 echo    "===================="
-read -p "hit return to upload " && curl -vF "image=@hoopla.ino.nodemcu.bin" http://$NAME/update
+#read -p "hit return to upload " && 
+curl -vF "image=@hoopla.ino.nodemcu.bin" http://$NAME/update
 echo
 
-echo    "==================="
-read -p "hit return to reset" && timeout 2 curl -v http://$NAME/debug/reset
+#echo    "==================="
+#read -p "hit return to reset" && timeout 2 curl -v http://$NAME/debug/reset
