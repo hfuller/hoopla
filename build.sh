b221200d1ae419d3475ffdd8849dc174b45a2494
Hunter Fuller <hfuller@pixilic.com>
2016-12-02 19:13:12 -0600

    build script reads the hostname from config.h

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+NAME="$(grep '#define NAME' config.h | cut -d '"' -f 2)"
+
 #terrible build script
 ~/Downloads/arduino-1.6.7/arduino hoopla.ino
 echo    "===================="
-read -p "hit return to upload " && curl -vF "image=@hoopla.ino.nodemcu.bin" http://jennifers-hoop/update
+read -p "hit return to upload " && curl -vF "image=@hoopla.ino.nodemcu.bin" http://$NAME/update
 echo
 
 echo    "==================="
-read -p "hit return to reset" && timeout 2 curl -v http://jennifers-hoop/debug/reset
+read -p "hit return to reset" && timeout 2 curl -v http://$NAME/debug/reset
