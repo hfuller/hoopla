8b66f26c83923f05ae34168b8b2035fc33e48d31
Hunter Fuller <hfuller@pixilic.com>
2016-12-11 00:10:33 -0600

    Add globals file that should have been in here before

---
+++ globals.h
@@ -0,0 +1,10 @@
+#include <FastLED.h>
+
+extern CRGB color;
+extern CRGB nextColor;
+extern CRGB leds[300];
+extern int numpixels;
+extern bool boolEffectState;
+extern int intEffectState;
+extern CRGBPalette16 currentPalette;
+
